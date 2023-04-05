56ae6e0fd8a407d9b63b809f5d420ee650843cbc
Vince Lee <vincelee@Vinces-Air.lan>
2023-04-05 15:56:07 -0700

    Add files.

---
+++ Sources/EQAutoLayout/EQAutoLayout.swift
@@ -0,0 +1,94 @@
+//
+//  EQAutolayout.swift
+//  EQAutolayout
+//
+//  Created by Vince Lee on 12/12/2022.
+//
+
+import UIKit
+
+///
+/// Creates custom operators to define autolayout constraints in a readable format:
+///
+///  Example:
+///  ````
+///  NSLayoutConstraint.activate([
+///     view1.topAnchor     .= view2.topAnchor,
+///     view1.bottomAnchor  .= view2.bottomAnchor - 12.0,
+///     view1.leadingAnchor .= view2.leadingAnchor + 10.0,
+///     view1.widthAnchor   .= 100.0
+///  ])
+
+infix operator .=: AssignmentPrecedence
+
+// MARK: - Functions
+
+// Set one anchor to another
+// e.g. view1.topAnchor .= view2.topAnchor
+public func .=<T>(lhs: NSLayoutAnchor<T>, rhs: NSLayoutAnchor<T>) -> NSLayoutConstraint {
+    return lhs.constraint(equalTo: rhs)
+}
+
+// Set one dimension to another
+// e.g. view1.widthAnchor .= view2.widthAnchor
+public func .=(lhs: NSLayoutDimension, rhs: NSLayoutDimension) -> NSLayoutConstraint {
+    return lhs.constraint(equalTo: rhs)
+}
+
+// Set dimension to constant
+// e.g. view1.heightAnchor .= 50.0
+public func .=(lhs: NSLayoutDimension, rhs: CGFloat) -> NSLayoutConstraint {
+    return lhs.constraint(equalToConstant: rhs)
+}
+
+// Set anchor to anchor plus constant
+// e.g. view1.topAnchor .= view2.topAnchor + 123
+public func .=<T>(lhs: NSLayoutAnchor<T>, rhs: EQAutolayoutAnchorIntermediate<T>) -> NSLayoutConstraint {
+    return lhs.constraint(equalTo: rhs.anchor, constant: rhs.constant)
+}
+
+// Set dimension to dimension plus constant
+// e.g. view1.widthAnchor .= view2.widthAnchor + 123
+public func .=(lhs: NSLayoutDimension, rhs: EQAutolayoutDimensionIntermediate) -> NSLayoutConstraint {
+    return lhs.constraint(equalTo: rhs.dimension, constant: rhs.constant)
+}
+
+// MARK: - Intermediate values (Anchors)
+
+// Container to hold intermediate result
+public struct EQAutolayoutAnchorIntermediate<T: AnyObject> {
+    let anchor: NSLayoutAnchor<T>
+    let constant: CGFloat
+}
+
+public func +<T>(lhs: NSLayoutAnchor<T>, rhs: CGFloat) -> EQAutolayoutAnchorIntermediate<T> {
+    return EQAutolayoutAnchorIntermediate(anchor: lhs, constant: rhs)
+}
+
+public func +<T>(lhs: CGFloat, rhs: NSLayoutAnchor<T>) -> EQAutolayoutAnchorIntermediate<T> {
+    return EQAutolayoutAnchorIntermediate(anchor: rhs, constant: lhs)
+}
+
+public func -<T>(lhs: NSLayoutAnchor<T>, rhs: CGFloat) -> EQAutolayoutAnchorIntermediate<T> {
+    return EQAutolayoutAnchorIntermediate(anchor: lhs, constant: -rhs)
+}
+
+// MARK: - Intermediate values (Dimension)
+
+// Container to hold intermediate result
+public struct EQAutolayoutDimensionIntermediate {
+    let dimension: NSLayoutDimension
+    let constant: CGFloat
+}
+
+public func +(lhs: NSLayoutDimension, rhs: CGFloat) -> EQAutolayoutDimensionIntermediate {
+    return EQAutolayoutDimensionIntermediate(dimension: lhs, constant: rhs)
+}
+
+public func +(lhs: CGFloat, rhs: NSLayoutDimension) -> EQAutolayoutDimensionIntermediate {
+    return EQAutolayoutDimensionIntermediate(dimension: rhs, constant: lhs)
+}
+
+public func -(lhs: NSLayoutDimension, rhs: CGFloat) -> EQAutolayoutDimensionIntermediate {
+    return EQAutolayoutDimensionIntermediate(dimension: lhs, constant: -rhs)
+}
---
+++ Tests/Test.swift
@@ -0,0 +1,8 @@
+//
+//  Test.swift
+//  
+//
+//  Created by Vince Lee on 4/5/23.
+//
+
+import Foundation
